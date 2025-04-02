63f6dfd6c0f4d5aae2f4f63f1f8e0d7bbb1352f8
pawanAXiX <pawanregmi86@gmail.com>
2025-04-03 01:00:45 +0545

    furfuri

--- resources/js/page/Main.vue
+++ resources/js/page/Main.vue
@@ -2,10 +2,10 @@
     <div class="px-50">
     <div class="w-full h-full px-5 py-5 text-white mx-auto">
         <div class="grid grid-cols-4 px-2 gap-5 py-4   bg-[#1E2022] border border-gray-100 rounded-lg ">
-                <DraggableColumn v-model:myArray="notStartedTasks.data" colName="Not Started"/>
-                <DraggableColumn v-model:myArray="inProgressTasks.data" colName="In Progress"/>
-                <DraggableColumn v-model:myArray="doneTasks.data" colName="Done"/>
-                <DraggableColumn v-model:myArray="archivedTasks.data" colName="Archived"/>
+                <DraggableColumn v-model:myArray="test.data" colName="Not Started"/>
+                <DraggableColumn v-model:myArray="inProgressTasks" colName="In Progress"/>
+                <DraggableColumn v-model:myArray="doneTasks" colName="Done"/>
+                <DraggableColumn v-model:myArray="archivedTasks" colName="Archived"/>
         </div>
     </div>
     </div>
@@ -16,24 +16,28 @@
 
 
 import axios from "axios";
-import {onMounted, ref, watch} from "vue";
+import {onMounted, reactive, ref, watch} from "vue";
 import DraggableColumn from "../componenet/DraggableColumn.vue";
 const lists=ref({});
-const notStartedTasks=ref({status:0,data:[]});
+const notStartedTasks=ref([]);
 
-const archivedTasks=ref({status:0,data:[]});
-const inProgressTasks=ref({status:0,data:[]});
-const doneTasks=ref({status:0,data:[]})
+const archivedTasks=ref([]);
+const inProgressTasks=ref([]);
+const doneTasks=ref([])
+const test=reactive({
+    status:0,
+    data:[]
+})
 const storeOrdering=ref('');
 const fetchTasks=async ()=>{
     const items=await axios.get('http://localhost:8000/api/tasks')
     lists.value=items.data.data;
     const data=Object.groupBy(lists.value,({status})=>status);
-    notStartedTasks.value.data=data[0];
-    inProgressTasks.value.data=data[1];
-    doneTasks.value.data=data[2];
-    archivedTasks.value.data=data[3];
-
+    // notStartedTasks.value=data[0];
+    test.data=data[0]
+    inProgressTasks.value=data[1];
+    doneTasks.value=data[2];
+    archivedTasks.value=data[3];
 
 }
 
@@ -46,7 +50,12 @@ const pushOrder=async ()=>{
     // send data to database
 }
 
-watch([notStartedTasks,inProgressTasks,doneTasks,archivedTasks],(newArrays,oldArrays)=>{
-
+watch([test.data,inProgressTasks,doneTasks,archivedTasks],(newArrays,oldArrays)=>{
+    // newArrays.map((item)=>{
+    //     item.map((task)=>{
+    //
+    //     })
+    // })
+    console.log(newArrays);
 },)
 </script>
